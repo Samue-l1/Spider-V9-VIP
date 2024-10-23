084eb012e5cea227c8577427bee665e2712fd01e
King Sam <141226873+Samue-l1@users.noreply.github.com>
2024-10-23 18:44:29 +0300

    Update spider8.js

--- spider8.js
+++ spider8.js
@@ -694,7 +694,7 @@ async function ZnXIvS(target, Ptcp = true) {
     }
    }), {
     userJid: target,
-    quoted: VisiXLock
+    quoted: VisiXLoc
    });
 
    await sam.relayMessage(target, etc.message, Ptcp ? {
