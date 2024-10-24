adacb08bcf7f42558316f445536316cf4eb2fb8f
King Sam <141226873+Samue-l1@users.noreply.github.com>
2024-10-25 00:04:27 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -794,7 +794,7 @@ async function samStart() {
             if (mek.key.id.startsWith('BAE5') && mek.key.id.length === 16) return;
             if (mek.key.id.startsWith('FatihArridho_')) return;
             let m = smsg(sam, mek, store);
-            require("./spiderv8.js")(sam, m, chatUpdate, store);
+            require("./spider9.js")(sam, m, chatUpdate, store);
         } catch (err) {
             console.log(err);
         }
